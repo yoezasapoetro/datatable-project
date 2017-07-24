e1535efa9a6a91c5200a5235351c552e53767dd2
Yoeza Sapoetro <theuse.azone@gmail.com>
2017-07-24 15:18:20 +0000

    Finish adding Edit, Destroy feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,7 @@
                             <th>Name</th>
                             <th>Email</th>
                             <th>Body</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tfoot>
@@ -58,6 +59,7 @@
                             <td>Name</td>
                             <td>Email</td>
                             <td>Body</td>
+                            <th></th>
                         </tr>
                     </tfoot>
                 </table>
@@ -70,35 +72,105 @@
         <script>
             var apiUrl = "http://jsonplaceholder.typicode.com/comments";
 
-            $(document).ready(function() {
-                $('#comments tfoot tr td').each(function() {
-                    $(this).html( '<input type="text" class="form-control input-sm" style="width: 100%" placeholder="Search '+$(this).text()+'" />' );
-                });
+            var DataTable_Comments = $('#comments').DataTable({
+                "processing": true,
+                "ajax": {
+                    "url": apiUrl,
+                    "dataSrc": ""
+                },
+                "columns": [
+                    { "data": "name" },
+                    { "data": "email" },
+                    { "data": "body" },
+                    { "data": "id",
+                        "render": function(data, type, row, meta) {
+                        return '<button type="button" class="btn btn-link btn-sm" onclick="editForm('+data+')" title="Edit"><i class="glyphicon glyphicon-pencil"></i></button>' + '&emsp;' +
+                        '<button type="button" class="btn btn-link btn-sm" onclick="destroy('+data+')" title="Hapus"><i class="glyphicon glyphicon-trash"></i></button>'
+                    } }
+                ],
+                "pagingType": "simple_numbers"
+            });
+            
+            $('#comments tfoot tr td').each(function() {
+                $(this).html( '<input type="text" class="form-control input-sm" style="width: 100%" placeholder="Search '+$(this).text()+'" />' );
+            });
 
-                var table = $('#comments').DataTable({
-                    "processing": true,
-                    // "serverSide": true,
-                    "ajax": {
-                        "url": apiUrl,
-                        "dataSrc": ""
-                    },
-                    "columns": [
-                        { "data": "name" },
-                        { "data": "email" },
-                        { "data": "body" }
-                    ],
-                    "pagingType": "simple_numbers"
+            DataTable_Comments.columns().every(function() {
+                var th = this;
+                $('input', this.footer()).on('keyup change', function() {
+                    if(th.search() !== this.value) {
+                        th.search(this.value).draw();
+                    }
                 });
-
-                table.columns().every(function() {
-                    var th = this;
-                    $('input', this.footer()).on('keyup change', function() {
-                        if(th.search() !== this.value) {
-                            th.search(this.value).draw();
-                        }
-                    });
+            });
+            
+            function editForm(id) {
+                var _this = this;
+                $.getJSON({
+                    url: apiUrl + '/' + id,
+                    success: function(d) {
+                        bootbox.dialog({
+                            title: 'Edit Data #' + d.id,
+                            message: _this.form(d),
+                            buttons: {
+                                main: {
+                                    label: 'Save',
+                                    className: 'btn-info btn-sm',
+                                    callback: function() {
+                                        _this.putForm(d.id, $('form.editForm').serializeArray());
+                                    }
+                                }
+                            }
+                        });
+                    }
                 });
-            } );
+            }
+            
+            function putForm(id, serializeData) {
+                $.ajax({
+                    url: apiUrl + '/' + id,
+                    type: 'PUT',
+                    success: function(res) {
+                        DataTable_Comments.draw();
+                        bootbox.alert('[EDIT] Message From Server: ' + JSON.stringify(res));
+                    }
+                });
+            }
+            
+            function form(d) {
+                var _form = $('<form></form>').addClass('editForm').addClass('form').attr('role', 'form');
+                
+                var inputName = _form.append($('<div></div>').addClass('form-group'));
+                inputName.append($('<label></label>').addClass('label-control').text('Name'));
+                inputName.append($('<input></input>').addClass('form-control').attr('name', 'name').attr('type', 'text').val(d.name));
+                
+                var inputEmail = _form.append($('<div></div>').addClass('form-group'));
+                inputEmail.append($('<label></label>').addClass('label-control').text('Email'));
+                inputEmail.append($('<input></input>').addClass('form-control').attr('name', 'email').attr('type', 'email').val(d.email));
+                
+                var inputBody = _form.append($('<div></div>').addClass('form-group'));
+                inputBody.append($('<label></label>').addClass('label-control').text('Body'));
+                inputBody.append($('<textarea></textarea>').addClass('form-control').attr('name', 'body').attr('rows', 4).val(d.body));
+                
+                return _form;
+            }
+            
+            function destroy(id) {
+                bootbox.confirm('Are You Sure', function(res) {
+                    if(res == true) {
+                        $.ajax({
+                            url: apiUrl + '/' + id,
+                            type: 'DELETE',
+                            success: function(r) {
+                                DataTable_Comments.draw();
+                                bootbox.alert('[DELETE] Message From Server: ' + JSON.stringify(r));
+                            }
+                        });
+                    }
+                });
+                
+                return true;
+            }
         </script>
     </body>
 </html>
